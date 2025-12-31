e2c8fdf2a24a3c6791186ebbec1682c6cde3a6fd
DESKTOP-4E9T2MH\Admin <ajoy9532@gmail.com>
2025-12-31 20:52:50 +0600

    added client side

---
+++ src/data/navlinks.ts
@@ -0,0 +1,8 @@
+import type { INavLink } from "../types";
+
+export const navlinks: INavLink[] = [
+  { name: "Home", href: "/" },
+  { name: "Generate", href: "/" },
+  { name: "My Generations", href: "/" },
+  { name: "My Content", href: "/" },
+];
